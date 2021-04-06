2eaa5dce6d9a8d8f47b5848e093e1806fec1b354
Imed Ben Kalia <imedbenkalia98@gmail.com>
2021-04-06 23:54:51 +0100

    add hero and nav filter and begin in product list component

--- src/Components/categorie/CollectionFilterNav.js
+++ src/Components/categorie/CollectionFilterNav.js
@@ -1,56 +1,72 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
 import Typography from "@material-ui/core/Typography";
+import Grid from '@material-ui/core/Grid';
+import Select from 'react-select';
+import { useState } from 'react';
+import Hidden from '@material-ui/core/Hidden';
 
 const useStyles = makeStyles((theme) => ({
     
 
 }))
-
+const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+];
 
 function CollectionFilterNav() {
+    const [selectedOption, setSelectOption] = useState(options[0])
+
+    const handlechange = selectedOption => { 
+        setSelectOption(selectedOption)
+        console.log(`Option selected: `, selectedOption)
+    };
+    
     return (
-        <div>
-            <Paper>
-                <Box>
-                    <Grid container>
-                        <Grid item>
-                            <Grid conatiner>
-                                <Grid item>
-                                    <Typography>
-                                        <Box>
-                                            FILTER BY
-                                        </Box>
-                                    </Typography>
-                                </Grid>
-                                <Grid item></Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item>
-                            <Grid conatiner>
-                                <Grid item>
-                                    <Typography>
-                                        <Box>
-                                            FILTER BY
-                                        </Box>
-                                    </Typography>
-                                </Grid>
-                                <Grid item></Grid>
-                            </Grid>
-                        </Grid>
-                        <Grid item>
-                            <Typography>
-                                <Box>
-                                    FILTER BY
-                                </Box>
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Box>
-            </Paper>
-        </div>
+        <Paper elevation={0} style={{borderBottom:"1px solid rgba(0, 0, 0, 0.12)"}}>
+            <Grid container justify="center" alignItems="center" >
+                
+                <Grid item justify='flex-end' >
+                    <Box height="100%" display="flex" alignItems="center">
+                        <Box component="div" p={1} m={1} bgcolor="background.paper">
+                            inline
+                        </Box>
+                        <Box  width={150} component="div" p={1} m={1} bgcolor="background.paper">
+                            <Select
+                                value={selectedOption}
+                                onChange={handlechange}
+                                options={options}
+                            />
+                        </Box> 
+                    </Box>
+                    
+                </Grid>
+                <Grid item justify='flex-end' >
+                    <Box height="100%" display="flex" alignItems="center">
+                        <Box component="div" p={1} m={1} bgcolor="background.paper">
+                            inline
+                        </Box>
+                        <Box  width={150} component="div" p={1} m={1} bgcolor="background.paper">
+                            <Select
+                                value={selectedOption}
+                                onChange={handlechange}
+                                options={options}
+                            />
+                        </Box> 
+                    </Box>
+                    
+                </Grid>
+                <Grid item lg={6} md={4} sm={3} xs={0}></Grid>
+                <Hidden xsDown>
+                    <Grid item>11 Items</Grid>
+                </Hidden>
+            </Grid>
+            
+            
+        </Paper>
     )
 }
 
--- src/pages/CategoryPage.js
+++ src/pages/CategoryPage.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import CollectionFilterNav from '../Components/categorie/CollectionFilterNav'
 import CollectionHero from '../Components/categorie/CollectionHero'
 
 function CategoryPage() {
     return (
+      <>
         <CollectionHero />
-    )
+        <CollectionFilterNav />
+      </>
+    );
 }
 
 export default CategoryPage
